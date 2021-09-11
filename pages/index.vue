482a17515ad9a68fa4c4ebb0896e2dec31744d85
Erik <erik.wubbels@gmail.com>
2021-09-11 11:44:47 +0200

    adding files

---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="main">
+    <b-navbar>
+      <b-navbar-brand href="#">Rusty Goobers</b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="https://discord.gg/bGBm7QZB">
+          <img src="/Discord-Logo.png" alt="" class="discord-logo">
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="container text-center">
+      <h1>Under Construction</h1>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="container wipe-schedule text-center">
+      <h2>Last Full Wipe:</h2>
+      <h3>Friday 10/9 15:00 CEST</h3>
+    </div>
+    <div class="container wipe-schedule text-center">
+      <h2>Next Full Wipe:</h2>
+      <h3>Sunday 12/9 15:00 CEST</h3>
+    </div>
+    <div class="footer text-center">
+      &copy; Rusty Goobers 2021
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+  .footer {
+    bottom: 0;
+    position: absolute;
+    width: 100%;
+  }
+  .footer div {
+    margin: auto;
+  }
+  .discord-logo {
+    max-height: 40px;
+  }
+</style>
